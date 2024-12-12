8c74758c2bc9e53b053f8cd3d1572d7f79ffdd7b
itsHikariii <hasonanthonynugroho88@gmail.com>
2024-12-12 07:44:13 +0700

    Remove SQLite

--- REV_UTS_72220538_Mobile/App.xaml.cs
+++ REV_UTS_72220538_Mobile/App.xaml.cs
@@ -7,14 +7,10 @@ namespace REV_UTS_72220538_Mobile
 {
     public partial class App : Application
     {
-        public static DatabaseService Database { get; private set; }
 
         public App()
         {
             InitializeComponent();
-
-            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "categories.db3");
-            Database = new DatabaseService(dbPath);
 
             MainPage = new MainPage(); // Atur halaman utama
         }
--- REV_UTS_72220538_Mobile/Data/category.cs
+++ REV_UTS_72220538_Mobile/Data/category.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SQLite;
+
 namespace REV_UTS_72220538_Mobile.Data
 {
 
